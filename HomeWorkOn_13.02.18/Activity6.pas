46c5836fc92a21888a0d2f50a9fb7977a905b006
Artem <zhimovskij@yandex.ru>
2018-02-09 18:34:08 +0500

    Update File: HomeWorkOn_13.02.18/Activity6.pas
    Bring the file "Activity6.pas" to completion.

--- HomeWorkOn_13.02.18/Activity6.pas
+++ HomeWorkOn_13.02.18/Activity6.pas
@@ -2,5 +2,23 @@
 a, b : integer;
 
 begin
+writeln('Enter number 1:');
+readln(a);
 
+writeln('Enter number 2:');
+readln(b);
+
+writeln('---------------------------------');
+
+writeln('Output sum:');
+writeln(a + b);
+
+writeln('Output difference:');
+writeln(a - b);
+
+writeln('Output multiplication:');
+writeln(a * b);
+
+writeln('Output division:');
+writeln(a / b);
 end.
