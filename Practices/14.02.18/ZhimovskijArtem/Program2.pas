98c451d3d317195d6ecef6077d085044955819a1
Artem <zhimovskij@yandex.ru>
2018-02-14 18:10:21 +0500

    Changes in structure and add files on practice work "Program1.pas", "Program2.pas"

---
+++ Practices/14.02.18/ZhimovskijArtem/Program2.pas
@@ -0,0 +1,13 @@
+﻿var 
+x,y, sum : double;
+begin
+WriteLn('Введите число:');
+ReadLn (x);
+
+if x<4 then sum := x*x-10 
+else if x >= 4 then sum:= sqrt (x+5) 
+else  sum:=x ;
+
+WriteLn('Вывод: ');
+WriteLn ('у = ', sum);
+end.
