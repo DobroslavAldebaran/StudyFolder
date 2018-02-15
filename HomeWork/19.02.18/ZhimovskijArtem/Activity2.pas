ffd92601d9ffc26dd4a96d057666c19db3dc8bbe
Artem <zhimovskij@yandex.ru>
2018-02-15 21:11:45 +0500

    Add files in folder "HomeWork/19.02.18/ZhimovskijArtem"
    add files "Activity1.pas" and "Activity2.pas". Homework complete.

---
+++ HomeWork/19.02.18/ZhimovskijArtem/Activity2.pas
@@ -0,0 +1,12 @@
+﻿var
+x : double;
+
+begin
+  Write('Введите число: ');
+  Read(x);
+  
+  x := (x < 0) ? Sqr(x) : (x > 8) ? Ln(x + 3) : Sqrt(x);
+  
+  WriteLn('-----------------------');
+  Write('Вывод: ', x : 3 : 2);
+end.
